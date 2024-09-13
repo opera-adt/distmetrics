f3a43579d0719b3d9b8613ce67f45737b119421d
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2024-09-13 09:19:04 -0700

    add get_device

--- src/distmetrics/__init__.py
+++ src/distmetrics/__init__.py
@@ -3,7 +3,7 @@ from .asf_io import read_asf_rtc_image_data
 from .despeckle import despeckle_one_rtc_arr_with_tv, despeckle_rtc_arrs_with_tv
 from .logratio import compute_log_ratio
 from .mahalanobis import compute_mahalonobis_dist_1d, compute_mahalonobis_dist_2d
-from .transformer import get_transformer_zscore, load_trained_transformer_model
+from .transformer import get_device, get_transformer_zscore, load_trained_transformer_model
 
 __all__ = [
     'compute_mahalonobis_dist_1d',
@@ -15,4 +15,5 @@ __all__ = [
     'compute_log_ratio',
     'load_trained_transformer_model',
     'get_transformer_zscore',
+    'get_device',
 ]
