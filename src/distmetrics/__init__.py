08b25b7114ded35ec0f651811355eb99702b8b83
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2024-09-24 14:33:44 -0700

    expose cusum

--- src/distmetrics/__init__.py
+++ src/distmetrics/__init__.py
@@ -1,5 +1,6 @@
 from .asf_burst_search import get_asf_rtc_burst_ts
 from .asf_io import read_asf_rtc_image_data
+from .cusum import compute_cusum_1d, compute_prob_cusum_1d
 from .despeckle import despeckle_one_rtc_arr_with_tv, despeckle_rtc_arrs_with_tv
 from .logratio import compute_log_ratio_decrease_metric
 from .mahalanobis import compute_mahalonobis_dist_1d, compute_mahalonobis_dist_2d
@@ -16,4 +17,6 @@ __all__ = [
     'load_trained_transformer_model',
     'compute_transformer_zscore',
     'get_device',
+    'compute_cusum_1d',
+    'compute_prob_cusum_1d'
 ]
