6483585f86adb1cde4cf26eff5be3e67f7d994b1
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2025-01-17 09:58:16 -0800

    get device update

--- src/distmetrics/transformer.py
+++ src/distmetrics/transformer.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import einops
 import numpy as np
+import platform
 import torch
 import torch.mps
 import torch.nn as nn
@@ -187,7 +188,7 @@ class SpatioTemporalTransformer(nn.Module):
 def get_device() -> str:
     if torch.cuda.is_available():
         device = 'cuda'
-    elif torch.backends.mps.is_available:
+    elif platform.system() == 'Darwin' and torch.backends.mps.is_available():
         device = 'mps'
     else:
         device = 'cpu'
