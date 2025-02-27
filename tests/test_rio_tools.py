873ccf8dd636af151cfe8f92154b1671b5b17efe
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2025-02-26 19:07:33 -0800

    update rasterio versin

--- tests/test_rio_tools.py
+++ tests/test_rio_tools.py
@@ -19,6 +19,7 @@ def test_merge_categorical_arrays(
         arrs,
         profiles,
         merge_method='min',
+        # Different rasterio/gdal versions may treat nodata slightly differently
         exterior_mask_dilation=10,
         # we are merging 2 datasets so need to specify the target crs as it can be random depending on which is chosen
         target_crs=profiles[1]['crs'],
@@ -27,8 +28,6 @@ def test_merge_categorical_arrays(
     with rasterio.open(categorical_merge_output_data) as ds:
         merged_array_expected = ds.read(1)
         merged_profile_expected = ds.profile
-    with rasterio.open('merged.tif', 'w', **merged_profile) as dst:
-        dst.write(merged_array, 1)
 
     assert_allclose(merged_array, merged_array_expected)
     assert merged_profile['crs'] == merged_profile_expected['crs']
